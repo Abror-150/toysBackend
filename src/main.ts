f7cd9e9cb42ab4cb7763708c4c0701421a8f2fed
Abror-150 <urazalievv.abror@gmail.com>
2026-01-17 15:18:50 +0500

    asdg

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
   });
   app.use('/images', express.static(path.join(process.cwd(), 'images')));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
